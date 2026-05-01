04d4e20ac95ea126c7dc7b0b78b0fa4592e2a425
Son Truong Bui <truongson832@gmail.com>
2026-05-02 01:20:55 +0700

    Add Game Feature plugins and Game Feature add pistol weapon

---
+++ Source/Gameplaylearning/CustomModularCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomModularCharacter.h"
+
+
+// Sets default values
+ACustomModularCharacter::ACustomModularCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACustomModularCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACustomModularCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACustomModularCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
--- Source/Gameplaylearning/Gameplaylearning.Build.cs
+++ Source/Gameplaylearning/Gameplaylearning.Build.cs
@@ -8,7 +8,7 @@ public class Gameplaylearning : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ModularGameplayActors" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
