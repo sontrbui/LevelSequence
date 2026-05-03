f9534d4961f2d479cebd04ad4ec6a8ab16e54e93
Son Truong Bui <truongson832@gmail.com>
2026-05-03 20:30:22 +0700

    Remove duplicate plugin files

--- Source/Gameplaylearning/Gameplaylearning.Build.cs
+++ Source/Gameplaylearning/Gameplaylearning.Build.cs
@@ -8,9 +8,9 @@ public class Gameplaylearning : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ModularGameplayActors" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameFeatureActions" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
